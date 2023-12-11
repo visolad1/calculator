42269e18f9cad1acfb39d68f9e41d5ab98feec65
--replace-all <visolad1@gmail.com>
2023-12-12 00:50:50 +0500

    changes: errors

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import styles from './Canvas.module.scss'
 import icon from '../../icons/canvas-icon.svg'
 import { Mode } from '../Mode'
+import { Droppable } from 'react-beautiful-dnd'
 
 export const Canvas = () => {
   return (
     <div>
-        <Mode />
-        <div className={styles.wrapper}>
-            <img src={icon} alt="icon" className={styles.icon}/>
+      <Mode />
+      <Droppable droppableId='componentsList'>
+        {(provided) => (
+          <div className={styles.wrapper} {...provided.droppableProps} ref={provided.innerRef}>
+            <img src={icon} alt="icon" className={styles.icon} />
             <h4 className={styles.title}>Перетащите сюда</h4>
             <p className={styles.subtitle}>любой элемент из левой панели</p>
-        </div>
-  </div>
+          </div>
+
+        )}
+      </Droppable>
+    </div>
   )
 }
--- src/components/SidebarComponents/index.tsx
+++ src/components/SidebarComponents/index.tsx
@@ -5,35 +5,86 @@ import { NumericKeypad } from '../NumericKeypad'
 import { ResultButton } from '../ResultButton'
 import styles from './SidebarComponents.module.scss'
 import { IMode, ModeContext } from '../../App'
+import { DragDropContext, Draggable, DropResult } from 'react-beautiful-dnd'
 
-export interface IComponentsDisable{
+export interface IComponentsDisable {
   componentsDisable: boolean,
   setComponentsDisable: (newState: boolean) => void
 }
 
-export const ComponentsDisableContext= React.createContext<IComponentsDisable | undefined>(undefined)
+export const ComponentsDisableContext = React.createContext<IComponentsDisable | undefined>(undefined)
+
+interface ComponentItem {
+  id: string;
+  component: React.ReactElement;
+}
+
+type ComponentsList = ComponentItem[];
+
+const componentsList: ComponentsList = [
+  {
+    id: '1',
+    component: <Display value={0} />,
+  },
+  {
+    id: '2',
+    component: <MathOperations />,
+  },
+  {
+    id: '3',
+    component: <NumericKeypad />,
+  },
+  {
+    id: '4',
+    component: <ResultButton />,
+  },
+];
+
 
 export const SidebarComponents = () => {
   const [componentsDisable, setComponentsDisable] = React.useState<boolean>(true);
-  const {mode} = React.useContext(ModeContext) as IMode;
-  console.log(componentsDisable)
+  const { mode } = React.useContext(ModeContext) as IMode;
+
+  const [list, setList] = React.useState(componentsList);
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    if (!destination) return
+
+    const items = Array.from(list);
+    const [newOrder] = items.splice(source.index, 1)
+    items.splice(destination.index, 0, newOrder)
+
+    setList(items)
+  }
 
   React.useEffect(() => {
     if (mode === 'constructor') {
       setComponentsDisable(true);
-    } else if(mode === 'runtime'){
+    } else if (mode === 'runtime') {
       setComponentsDisable(false)
     }
   }, [mode]);
 
   return (
-      <div className={styles.wrapper}>
-        <ComponentsDisableContext.Provider value={{componentsDisable, setComponentsDisable}}>
-            <div className={`${styles.component} ${componentsDisable && styles.disabled}`}><Display value={0}/></div>
-            <div className={`${styles.component} ${componentsDisable && styles.disabled}`}><MathOperations /></div>
-            <div className={`${styles.component} ${componentsDisable && styles.disabled}`}><NumericKeypad /></div>
-            <div className={`${styles.component} ${componentsDisable && styles.disabled}`}><ResultButton /></div>  
-          </ComponentsDisableContext.Provider>
-      </div>
+    <div className={styles.wrapper}>
+      <ComponentsDisableContext.Provider value={{ componentsDisable, setComponentsDisable }}>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {componentsList.map((item, index) => (
+            <Draggable draggableId={item.id} index={index}>
+              {(provided) => (
+                <div ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  key={item.id}
+                >
+                  {item.component}
+                </div>
+              )}
+            </Draggable>
+          ))}
+        </DragDropContext>
+      </ComponentsDisableContext.Provider>
+    </div>
   )
 }
