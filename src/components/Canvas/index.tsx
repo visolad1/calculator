8932f44a753f34e80d5cdebcc51b0339547d23f6
--replace-all <visolad1@gmail.com>
2023-12-18 01:39:52 +0500

    changes

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -3,7 +3,7 @@ import styles from './Canvas.module.scss';
 import isOverIcon from '../../icons/isover.svg'
 import { Mode } from '../Mode';
 import { useDrop } from 'react-dnd';
-import { CanvasContext } from '../../context';
+import { CanvasContext, ModeContext } from '../../context';
 import { ICanvas, IDraggableComponentProps, IMode } from '../../types';
 import { Box } from '../UI/Box';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
@@ -13,7 +13,7 @@ import { DraggableObj } from './DraggableObj';
 
 export const Canvas = () => {
   const { canvas, setCanvas } = React.useContext(CanvasContext) as ICanvas
-
+  const { mode } = React.useContext(ModeContext) as IMode
   const [{ isOver }, dropRef] = useDrop({
     // Принимаем объект с типом 'component'
     accept: 'component',
@@ -28,18 +28,13 @@ export const Canvas = () => {
       isOver: monitor.isOver(),
     }),
   });
-
-
   const canvasEmpty = canvas.length === 0
-
   canvas.forEach((elem, index) => {
     if (elem.id === 'display') {
       const display = canvas.splice(index, 1)[0];
       canvas.unshift(display)
     }
   })
-
-
   const handleDragEnd = (result: DropResult) => {
     if (!result.destination) {
       return;
@@ -59,9 +54,16 @@ export const Canvas = () => {
           <DroppableZone>
             {canvas.map((obj, index) => (
               <Box shadow={false}>
-                <DraggableObj id={obj.id} index={index}>
-                  {obj.children}
-                </DraggableObj>
+                {mode === 'constructor' && (
+                  <DraggableObj id={obj.id} index={index}>
+                    {obj.children}
+                  </DraggableObj>
+                ) || (
+                    <>
+                      {obj.children}
+                    </>
+                  )}
+
               </Box>
             ))}
           </DroppableZone>
